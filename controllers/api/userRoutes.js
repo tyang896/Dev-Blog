b7d6087be0e3ca0250deb64229b35a6e8fb23994
Talee Y <talee.yng@gmail.com>
2022-10-12 01:07:05 -0500

    Add working code to login and view dashboard

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
+const {User} = require('../../models')
 
-router.get('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    try {
+        console.log("this try works")
+        const userData = await User.findOne({ where: { username: req.body.username } });
     
-})
+        if (!userData) {
+          res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again' });
+          return;
+        }
+    
+        const validPassword = await userData.checkPassword(req.body.password);
+    
+        if (!validPassword) {
+          res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again' });
+          return;
+        }
+    
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+          
+          res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    
+      } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+      }
+});
+
+module.exports = router;
