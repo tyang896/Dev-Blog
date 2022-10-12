d2db78be0b9391c1783125e5aa64d230330af8f2
Talee Y <talee.yng@gmail.com>
2022-10-11 22:51:44 -0500

    Add login page handlebars

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,10 +1,56 @@
 const router = require('express').Router();
+const {User, Post} = require('../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try{
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
 
-router.get('/', (req, res) => {
-    res.render('homepage', {
-        logged_in: req.session.logged_in
-    });
+        const posts = postData.map((post) => post.get({plain: true}));
+        console.log(`This is the posts list: ${posts}`);
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+          attributes: { exclude: ['password'] },
+          include: [{ model: Post }],
+        });
+    
+        const user = userData.get({ plain: true });
+    
+        res.render('dashboard', {
+          ...user,
+          logged_in: true
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+})
+
+router.get('/login', (req, res) => {
+// If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
 })
 
 module.exports = router;
