4f070ddf6e647d2f1968ed547286525d0763133b
Talee Y <talee.yng@gmail.com>
2022-10-13 15:47:40 -0500

    Modify server.js file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: "super secret",//May need to update later
     cookie: {
         maxAge: 1800000,
         httpOnly: true,
